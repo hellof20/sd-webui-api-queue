6e0a9365a3e4e93262c7bda8916849b029fd5bea
Wenming Pan <pwm@google.com>
2023-09-15 17:02:29 +0000

    modify code

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
-from flask import Flask,request
-from sd_pub import pub_msg
+from flask import Flask,request, abort
 import redis
-import time
+import os, sys, time, json
+from loguru import logger
+from google.cloud import pubsub_v1
 
 app = Flask(__name__)
+
+log_level= os.getenv('LOG_LEVEL')
+topic_name = os.getenv('TOPIC_NAME')
+redis_host = os.getenv('REDIS_HOST')
+timeout = os.getenv('TIMEOUT', default = 20)
+
+if log_level == 'debug':
+    logger.remove()
+    logger.add(sys.stdout, level='DEBUG')
+else:
+    logger.remove()
+    logger.add(sys.stdout, level='INFO', format="<level>{time} | {level} | {message}</level>")
+
+def pub_msg(topic, msg):
+    publisher = pubsub_v1.PublisherClient()
+    future = publisher.publish(topic, json.dumps(msg).encode('utf-8'))
+    resp = future.result()
+    return resp
 
 @app.route('/sdapi/v1/<path:text>',methods = ['POST'])
 def sdapi(text):
@@ -11,12 +30,20 @@ def sdapi(text):
     data = {}
     data['path'] = request.path
     data['msg'] = request.json
-    input_topic = 'projects/speedy-victory-336109/topics/sd-input'
-    msg_id = pub_msg(msg = data, topic = input_topic)
-    r = redis.Redis(host='localhost', port=6379, db=0)
-    r.set(msg_id, '')
+    try:
+        msg_id = pub_msg(msg = data, topic = topic_name)
+        logger.debug("msg_id: " + msg_id)
+    except Exception as e:
+        logger.error('Write request to queue failed.')
+        abort(500)
+    try:
+        r = redis.Redis(host=redis_host, port=6379, db=0)
+        r.set(msg_id, '')
+    except Exception as e:
+        logger.error('Connect to Redis failed.')
+        abort(500)
     num = 0
-    while num < 10:
+    while num < int(timeout)*2:
         value = r.get(msg_id)
         if value != b'':
             result = value
@@ -24,8 +51,8 @@ def sdapi(text):
         time.sleep(0.5)
         num += 1
     else:
-        return 'timeout'
+        return 'Timeout'
     return result
 
 if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+    app.run(debug=True, port=8080, host='0.0.0.0')
---
+++ worker.py
@@ -0,0 +1,82 @@
+from google import pubsub_v1
+import requests, json, time, os, sys
+import redis
+from loguru import logger
+
+log_level= os.getenv('LOG_LEVEL')
+subscription = os.getenv('SUBSCRIPTION')
+redis_host = os.getenv('REDIS_HOST')
+sd_api = os.getenv('SD_API', default = 'http://127.0.0.1:7860')
+r = redis.Redis(host=redis_host, port=6379, db=0)
+
+if log_level == 'debug':
+    logger.remove()
+    logger.add(sys.stdout, level='DEBUG')
+else:
+    logger.remove()
+    logger.add(sys.stdout, level='INFO', format="<level>{time} | {level} | {message}</level>")
+
+## pull的方式
+client = pubsub_v1.SubscriberClient()
+def pull_msg():
+    request = pubsub_v1.PullRequest(
+        subscription = subscription,
+        max_messages = 1,
+        # return_immediately = True
+    )
+    response = client.pull(request=request,)
+    return response
+
+
+def acknowledge(ack_id):
+    request = pubsub_v1.AcknowledgeRequest(
+        subscription = subscription,
+        ack_ids=[ack_id],
+    )
+    client.acknowledge(request=request)
+
+
+def send_request_sd_api(msg):
+    msg = json.loads(msg.decode('utf-8'))
+    uri = msg['path']
+    parameters = msg['msg']
+    logger.debug(sd_api+uri)
+    logger.debug(parameters)
+    respone = requests.post(sd_api+uri, json=parameters)
+    return respone
+
+while True:
+    logger.info('Waiting request...')
+    try:
+        resp = pull_msg()
+        if bool(resp):              
+            ack_id = resp.received_messages[0].ack_id
+            msg = resp.received_messages[0].message.data
+            msg_id = resp.received_messages[0].message.message_id
+            logger.debug("msg_id: " + msg_id)
+            respone = send_request_sd_api(msg)
+            logger.debug(respone)
+            if respone.status_code == 200:
+                r.set(msg_id, respone.text)
+                acknowledge(ack_id)
+                logger.info('msg_id: %s process success.' % msg_id)
+                logger.info('---------------------------------------')
+        else:
+            logger.info('Currently no request')
+    except Exception as e:
+        logger.error("request process failed.")
+    time.sleep(1)
+
+## 订阅的方式
+# subscriber_client = pubsub_v1.SubscriberClient()
+# subscription = subscriber_client.subscription_path("speedy-victory-336109", "sd-input-pull")
+# def callback(message):
+#     print(message)
+#     message.ack()
+# future = subscriber_client.subscribe(
+#     subscription, callback)
+# try:
+#     future.result()
+# except KeyboardInterrupt:
+#     future.cancel()  # Trigger the shutdown.
+#     future.result()  # Block until the shutdown is complete.
